58ddb8c3fca33ee54634a2c292fdde89fe004b81
Braydon <foxden00@gmail.com>
2017-01-31 14:23:55 -0500

    Implemented Part3 of project.

--- Part3/Main.aspx
+++ Part3/Main.aspx
@@ -10,6 +10,40 @@
     <form id="form1" runat="server">
     <div>
     
+        <h1>Student Grade</h1>
+
+        Student Name:
+        <br />
+        <asp:TextBox ID="tbxStudentName" runat="server"></asp:TextBox>
+        
+        <br />
+        Total Quiz Score:
+        <br />
+        <asp:TextBox ID="tbxQuizScore" runat="server"></asp:TextBox>
+        
+        <br />
+        Total Assignment Score:
+        <br />
+        <asp:TextBox ID="tbxAssignmentScore" runat="server"></asp:TextBox>
+        
+        <br />
+        Midterm Exam Score:
+        <br />
+        <asp:TextBox ID="tbxMidtermScore" runat="server"></asp:TextBox>
+        
+        <br />
+        Final Exam Score:
+        <br />
+        <asp:TextBox ID="tbxFinalScore" runat="server"></asp:TextBox>
+        <br />
+        
+        <br />
+        <asp:Button ID="calculateButton" runat="server" Text="Calculate" OnClick="calculateButton_Click" />
+        <br />
+
+        <br />
+        <asp:Label ID="lblResult" runat="server" Text=""></asp:Label>
+    
     </div>
     </form>
 </body>
--- Part3/Main.aspx.cs
+++ Part3/Main.aspx.cs
@@ -13,5 +13,45 @@ namespace Part3
         {
 
         }
+
+        protected void calculateButton_Click(object sender, EventArgs e)
+        {
+            string name = tbxStudentName.Text;
+
+            double quizScore = double.Parse(tbxQuizScore.Text);
+            double assignmentScore = double.Parse(tbxAssignmentScore.Text);
+            double midtermScore = double.Parse(tbxMidtermScore.Text);
+            double finalScore = double.Parse(tbxFinalScore.Text);
+
+            double averageScore = (quizScore * .15) + (assignmentScore * .40) + (midtermScore * .20) + (finalScore * .25);
+
+            string letterGrade = computeLetterGradeFrom(averageScore);
+
+            lblResult.Text = "Grade for " + name + ": " + averageScore + "%  " + letterGrade;
+        }
+
+        private static string computeLetterGradeFrom(double averageScore)
+        {
+            if (averageScore >= 90)
+            {
+                return "A";
+            }
+            else if (averageScore >= 80)
+            {
+                return "B";
+            }
+            else if (averageScore >= 70)
+            {
+                return "C";
+            }
+            else if (averageScore >= 60)
+            {
+                return "D";
+            }
+            else
+            {
+                return "F";
+            }
+        }
     }
 }
